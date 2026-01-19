32892ecac2efd125909dee669110881990e92795
Claudio Moura <claudio.moura@sapo.pt>
2026-01-19 21:05:51 +0100

    Arreglada ruta en PublicNav apra q no pete

--- apps/web/src/components/layout/PublicNav.tsx
+++ apps/web/src/components/layout/PublicNav.tsx
@@ -1,7 +1,7 @@
-\"use client\";
+"use client";
 
-import Link from \"next/link\";
-import { useLanguage } from \"@/context/LanguageProvider\";
+import Link from "next/link";
+import { useLanguage } from "@/context/LanguageProvider";
 
 export default function PublicNav({ loggedIn }: { loggedIn: boolean }) {
   const { t } = useLanguage();
