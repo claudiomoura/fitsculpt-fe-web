59fdd45641780aa9efa5dc2e6561eb735ed8e4bd
Claudio Moura <claudio.moura@sapo.pt>
2026-02-19 21:19:33 +0100

    feat(web): add reusable trainer client 360 UI components

---
+++ apps/web/src/components/trainer-client/index.ts
@@ -0,0 +1,7 @@
+export * from "./AssignPlanModal";
+export * from "./ClientHeaderCard";
+export * from "./ClientProfileSummary";
+export * from "./ClientRecentActivityCard";
+export * from "./CreateNoteModal";
+export * from "./NotesPanel";
+export * from "./TrainerClientSkeletons";
