3654201785cd8e24646c3e51fb2030881d9dbbe5
Claudio Moura <claudio.moura@sapo.pt>
2026-03-11 16:05:33 +0100

    feat(web): add v0 base shell components

---
+++ apps/web/src/components/v0/index.ts
@@ -0,0 +1,4 @@
+export { V0ScreenShell } from "./layout/V0ScreenShell";
+export { V0HomeShell } from "./screens/V0HomeShell";
+export { V0TrainingShell } from "./screens/V0TrainingShell";
+export { V0NutritionShell } from "./screens/V0NutritionShell";
